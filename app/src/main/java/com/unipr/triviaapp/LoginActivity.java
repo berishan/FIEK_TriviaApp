99e2ea7e2da985fb367c312546630f1444a9034f
Nora <color.ui>
2021-06-29 21:36:19 +0200

    you can now stay logged in via shared preferences + code reformatting

--- app/src/main/java/com/unipr/triviaapp/LoginActivity.java
+++ app/src/main/java/com/unipr/triviaapp/LoginActivity.java
@@ -4,11 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
@@ -37,6 +39,7 @@ public class LoginActivity extends AppCompatActivity{
     private TextView twRegister, twForgotPassword;
     private FirebaseAuth mAuth;
     private ProgressBar progressBar;
+    private CheckBox cbRememberMe;
 
 
     private FirebaseUser firebaseUser;
@@ -54,6 +57,7 @@ public class LoginActivity extends AppCompatActivity{
         btnLogin = findViewById(R.id.btnLogin);
         twForgotPassword = findViewById(R.id.twForgotPassword);
         progressBar = findViewById(R.id.progressBar2);
+        cbRememberMe = findViewById(R.id.cbRememberMe);
 
         databaseReference = FirebaseDatabase.getInstance().getReference("Users");
 
@@ -83,6 +87,27 @@ public class LoginActivity extends AppCompatActivity{
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class));
+            }
+        });
+
+        cbRememberMe.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(buttonView.isChecked()){
+                    SharedPreferences sharedPreferences = getSharedPreferences("checkbox",MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("remember", "true");
+                    editor.apply();
+
+
+
+                }
+                else if(!buttonView.isChecked()){
+                    SharedPreferences sharedPreferences = getSharedPreferences("checkbox",MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("remember", "false");
+                    editor.apply();
+                }
             }
         });
 
